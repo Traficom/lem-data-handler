b11b5404c77c2508c88c66cb09b597ec1c604744
Sami Mäkinen <ext-sami.makinen@traficom.fi>
2023-11-01 12:29:10 +0200

    Add __init__.py to import commonly used classes

---
+++ src/model_data/__init__.py
@@ -0,0 +1,5 @@
+from .data_item import DataItem
+from .environment import Environment
+from .los import LOSData, LOSMatrix, LOSMode, LOSTimePeriod, TimePeriod
+from .module import Module, Phase
+from .zone_mapping import Zone, ZoneMapping
